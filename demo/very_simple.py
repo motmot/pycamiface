85e49910f1ea4f5b731e6904aba91636750c2d6f
Andrew Straw <strawman@astraw.com>
2009-04-30 10:59:02 -0700

    add very simple example

---
+++ demo/very_simple.py
@@ -0,0 +1,15 @@
+import pkg_resources
+import motmot.cam_iface.choose as cam_iface_choose
+import numpy as np
+
+# Choose the unity ctypes backend
+cam_iface = cam_iface_choose.import_backend( 'unity', 'ctypes' )
+
+mode_num = 0
+device_num = 0
+num_buffers = 32
+
+cam = cam_iface.Camera(device_num,num_buffers,mode_num)
+cam.start_camera()
+frame = np.asarray(cam.grab_next_frame_blocking())
+print 'grabbed frame with shape %s'%(frame.shape,)
