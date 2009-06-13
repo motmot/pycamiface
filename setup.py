88d5362820149ea34998bfef7a348a7a4e9380bb
Andrew Straw <strawman@astraw.com>
2009-06-13 13:55:44 -0700

    bump version for release

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(name='motmot.cam_iface',
       long_description="""cam_iface is the core packge of several that
 are involved with digital camera acquisition and analysis""",
       url='http://code.astraw.com/projects/motmot/cam_iface.html',
-      version='0.4.5',
+      version='0.4.6',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       license="BSD",
