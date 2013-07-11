11eb9804fce907c07f2079abc24aa204430d95f0
John Stowers <john.stowers@gmail.com>
2013-07-12 00:15:19 +0200

    make very_simple more useful

--- demo/very_simple.py
+++ demo/very_simple.py
@@ -1,7 +1,13 @@
 # Also the example in ../README.rst -- so keep in sync
 import pkg_resources
-import motmot.cam_iface.cam_iface_ctypes as cam_iface
+import pprint
 import numpy as np
+try:
+    from scipy.misc import imsave
+except ImportError:
+    imsave = None
+
+import motmot.cam_iface.cam_iface_ctypes as cam_iface
 
 mode_num = 0
 device_num = 0
@@ -9,5 +15,18 @@ num_buffers = 32
 
 cam = cam_iface.Camera(device_num,num_buffers,mode_num)
 cam.start_camera()
+
+print "vendor", cam_iface.get_camera_info(device_num)
+
+nt = cam.get_num_trigger_modes()
+print "n trigger modes", nt
+for i in range(nt):
+    print "trigger", cam.get_trigger_mode_string(i)
+
+for n in range(cam.get_num_camera_properties()):
+    pprint.pprint( cam.get_camera_property_info(n) )
+
 frame = np.asarray(cam.grab_next_frame_blocking())
 print 'grabbed frame with shape %s'%(frame.shape,)
+if frame is not None and imsave:
+    imsave("test.png", frame)
